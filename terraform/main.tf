8ed664fa3d6f7918c312267077b4e1ce2b1be5fc
Ubuntu <ubuntu@ip-172-31-39-201.eu-west-2.compute.internal>
2024-06-13 15:00:53 +0000

    Clean up terraform and remove superfluous SG

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,21 +31,6 @@ variable "docker_sha"  {
   default = ""
 }
 
-
-
-
-# No longer need this, using dockerhub repo
-#
-#resource "aws_ecr_repository" "my_first_ecr_repo" {
-#  name = "my-first-ecr-repo" # Naming my repository
-#}
-
-
-#resource "aws_ecs_cluster" "mongo_wrapper_cluster" {
-#  name = "mongo-wrapper-cluster" # Naming the cluster
-#}
-
-#      "image": "${aws_ecr_repository.my_first_ecr_repo.repository_url}",
 resource "aws_ecs_task_definition" "mongo_wrapper_task" {
   family                   = "mongo-wrapper-task" # Naming our first task
   container_definitions    = <<DEFINITION
@@ -104,31 +89,6 @@ resource "aws_ecs_task_definition" "mongo_wrapper_task" {
   execution_role_arn       = "arn:aws:iam::637423404396:role/AlexECSTaskExecutionRole"
 }
 
-#resource "aws_iam_role" "ecsTaskExecutionRole2" {
-#  name               = "ecsTaskExecutionRole2"
-#  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-#}
-#
-#data "aws_iam_policy_document" "assume_role_policy" {
-#  statement {
-#    actions = ["sts:AssumeRole"]
-#
-#    principals {
-#      type        = "Service"
-#      identifiers = ["ecs-tasks.amazonaws.com"]
-#    }
-#  }
-#}
-#
-#resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole2_policy" {
-#  role       = "${aws_iam_role.ecsTaskExecutionRole2.name}"
-#  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-#}
-
-#resource "aws_service_discovery_http_namespace" "namespace" {
-#  name        = "namespace"
-#  description = "Namespace for MD Service Discovery"
-#}
 
 resource "aws_ecs_service" "mongo_wrapper_service" {
   name            = "mongo-wrapper-service"                             # Naming our first service
@@ -138,10 +98,6 @@ resource "aws_ecs_service" "mongo_wrapper_service" {
   launch_type     = "FARGATE"
   desired_count   = 1 # Setting the number of containers we want deployed to 3
 
-#  service_connect_configuration {
-#    enabled   = "true"
-#  }
-
   load_balancer {
     #target_group_arn = "${aws_lb_target_group.mongo_wrapper_target_group.arn}" # Referencing our target group
     #target_group_arn = "arn:aws:elasticloadbalancing:eu-west-2:182028175464:targetgroup/md-wrapper-target-group/6a556b45daff89af"
@@ -149,22 +105,6 @@ resource "aws_ecs_service" "mongo_wrapper_service" {
     container_name   = "${aws_ecs_task_definition.mongo_wrapper_task.family}"
     container_port   = 3000 # Specifying the container port
   }
-
-#  service_connect_configuration {
-#    enabled   = true
-#    #namespace = aws_service_discovery_http_namespace.namespace.arn
-#    namespace = "monitor-namespace"
-#    service {
-#      discovery_name = "md-service"
-#      port_name      = "api-port"
-#      client_alias {
-#        dns_name = "md-wrapper-dns"
-#        port     = 3000
-#      }
-#    }
-#  }
-
-#  depends_on = [aws_lb_listener.listener]
 
   network_configuration {
     subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
@@ -181,7 +121,8 @@ resource "aws_security_group" "mongo_wrapper_service_security_group" {
     # Only allowing traffic in from the load balancer security group
     #security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
 #    security_groups = ["sg-039063a37c674e76b"]
-    security_groups = ["sg-0171f41902e1d2b44", "sg-05edc1b1e4b5cd1e2"]
+    #   MD LB SG     
+    security_groups = ["sg-05edc1b1e4b5cd1e2"]
 
 
   }
@@ -210,62 +151,5 @@ resource "aws_default_subnet" "default_subnet_b" {
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "eu-west-2c"
 }
-
-
-#
-# The Load balancer
-#
-#resource "aws_alb" "mongo_wrapper_load_balancer" {
-#  name               = "mongo-wrapper-lb-tf" # Naming our load balancer
-#  load_balancer_type = "application"
-#  subnets = [ # Referencing the default subnets
-#    "${aws_default_subnet.default_subnet_a.id}",
-#    "${aws_default_subnet.default_subnet_b.id}",
-#    "${aws_default_subnet.default_subnet_c.id}"
-#  ]
-#  # Referencing the security group
-#  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-#}
-
-# Creating a security group for the load balancer:
-#resource "aws_security_group" "load_balancer_security_group" {
-#  ingress {
-#    from_port   = 3000 # Allowing traffic in from port 80
-#    to_port     = 3000
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-#  }
-#
-#  egress {
-#    from_port   = 0 # Allowing any incoming port
-#    to_port     = 0 # Allowing any outgoing port
-#    protocol    = "-1" # Allowing any outgoing protocol 
-#    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
-#  }
-#}
-
-#resource "aws_lb_target_group" "mongo_wrapper_target_group" {
-#  name        = "target-group"
-#  port        = 3000
-#  protocol    = "HTTP"
-#  target_type = "ip"
-#  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
-#  health_check {
-#    matcher = "200,301,302"
-#    path = "/motraffic"
-#  }
-#}
-
-#resource "aws_lb_listener" "listener" {
-#  load_balancer_arn = "${aws_alb.mongo_wrapper_load_balancer.arn}" # Referencing our load balancer
-#  port              = "3000"
-#  protocol          = "HTTP"
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = "${aws_lb_target_group.mongo_wrapper_target_group.arn}" # Referencing our target group
-#  }
-#}
-
-
 
 
